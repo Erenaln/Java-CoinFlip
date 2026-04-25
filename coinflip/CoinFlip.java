45dc1380913aba5573e629efd983bcf083f7bf9c
Erenaln <erenvalan@gmail.com>
2026-04-25 17:17:26 +0300

    Add files via upload

---
+++ coinflip/CoinFlip.java
@@ -0,0 +1,18 @@
+package coinflip;
+
+import java.util.Random;
+
+public class CoinFlip {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        
+        boolean isHeads = random.nextBoolean();
+        if (isHeads == true) {
+            System.out.println("Heads");
+        } else {
+            System.out.println("Tails");
+        }
+    }
+    
+}
